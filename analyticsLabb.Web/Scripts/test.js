4b4faf8ab15c5adad8cab81a6975660c335b8e62
emra <emma.rangert@gmail.com>
2014-04-29 16:12:22 +0200

    add ecommerce tracking

---
+++ analyticsLabb.Web/Scripts/test.js
@@ -0,0 +1,38 @@
+﻿
+
+$(document).ready(function () {
+    $(function () {
+        $("#tabs").tabs();
+    });
+
+    $('#formet').click(function () {
+
+        var id = Math.floor((Math.random() * 1000 + 1));
+
+        ga('ecommerce:addTransaction', {
+            'id': id,                     // Transaction ID. Required.
+            'affiliation': 'Acme Clothing',   // Affiliation or store name.
+            'revenue': '11.99',               // Grand Total.
+            'shipping': '5',                  // Shipping.
+            'tax': '1.29'                     // Tax.
+        });
+
+        ga('ecommerce:addItem', {
+            'id': id,                     // Transaction ID. Required.
+            'name': 'Fluffy Pink Bunnies',    // Product name. Required.
+            'sku': 'DD23444',                 // SKU/code.
+            'category': 'Party Toys',         // Category or variation.
+            'price': '11.99',                 // Unit price.
+            'quantity': '1'                   // Quantity.
+        });
+
+        console.log("ga");
+        ga('send', 'event', 'formulär', 'skickat', 'kontaktformulär', { 'hitCallback': function () { document.location = '/'; } });
+        return false;
+    });
+
+
+    
+
+
+});
